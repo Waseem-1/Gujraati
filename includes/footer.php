27c454e4cf52960130f2f7faf5423e45e01526e1
Waseem <wahmed@uni-koblenz.de>
2020-08-13 15:55:30 +0200

    Added css and js

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,3 @@
 Copyrights 2020
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="includes/js/main.js"></script>
---
+++ includes/js/main.js
@@ -0,0 +1,6 @@
+function init() {
+  console.log("jquery worked");
+}
+$(document).ready(function () {
+  init();
+});
