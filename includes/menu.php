c09cb3aec69a005eaae3c9f41f8d8b05bb9f9b39
Waseem <wahmed@uni-koblenz.de>
2020-08-13 15:27:23 +0200

    adds basic pages

--- includes/menu.php
+++ includes/menu.php
@@ -1,6 +1,6 @@
 <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Product</a></li>
-    <li><a href="#">Contact</a></li>
+    <li><a href="/index.php">Home</a></li>
+    <li><a href="/about.php">About</a></li>
+    <li><a href="/product.php">Product</a></li>
+    <li><a href="/contact.php">Contact</a></li>
 </ul>
